94e2a48a6218d432684deae817cd5e41d4929cd8
T-anh2302 <vtuananhh@gmail.com>
2024-03-20 23:53:54 +0700

    65 REACT TOASTIFY

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -55,7 +56,7 @@ function Column({ column }) {
 
   const addNewCard = () => {
     if (!newCardTitle) {
-      // console.error('Please enter Card Title')
+      toast.error('Please enter Card Title', { position: 'bottom-right' })
       return
     }
 
